995330f6224f177d843a7a1d96a03103f248b82c
Alessandro Pinto <alessandro.pinto@de.akelius.com>
2023-09-27 13:13:00 +0200

    thanks page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SurveyPageComponent } from './pages/survey-page/survey-page.component';
+import { ThanksPageComponent } from './pages/thanks-page/thanks-page.component';
 
 const routes: Routes = [
   {
     path: '',
     component: SurveyPageComponent,
+  },
+  {
+    path: 'thanks',
+    component: ThanksPageComponent,
   },
 ];
 
---
+++ src/app/pages/thanks-page/thanks-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-thanks-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './thanks-page.component.html',
+  styleUrls: ['./thanks-page.component.scss']
+})
+export class ThanksPageComponent {
+
+}
