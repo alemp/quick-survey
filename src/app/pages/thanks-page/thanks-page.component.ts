71b05f8f7fe31848874e2055ec453170699d9798
Alessandro Pinto <alessandro.pinto@de.akelius.com>
2023-09-28 14:29:55 +0200

    added logo to thanks page

--- src/app/pages/thanks-page/thanks-page.component.ts
+++ src/app/pages/thanks-page/thanks-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
+import { LogoComponent } from '../../components/logo/logo.component';
 
 @Component({
   selector: 'app-thanks-page',
   standalone: true,
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, TranslateModule, LogoComponent],
   templateUrl: './thanks-page.component.html',
   styleUrls: ['./thanks-page.component.scss'],
 })
